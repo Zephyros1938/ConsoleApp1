02a6cdf0d39fda6375878f873bfbc42c00446387
Xtrau505 <xtrau505true@gmail.com>
2025-01-16 07:41:47 -0600

    Add docs

---
+++ docs/potentialassets.md
@@ -0,0 +1,11 @@
+# Potential Assets
+
+A general list of potential assets for the game engine
+
+## Sounds
+
+### Youtube
+
+- https://www.youtube.com/watch?v=MtPrv5tKZgk
+
+### Extern
--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032af456a74c164c6009897d819816968854f563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6258e600b3805866498854697cce49b51bca3cd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
